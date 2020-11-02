66af562bb1c6234a82a58b7192e72b022f738232
ore291 <oreoluwapadonu@gmail.com>
2020-11-02 19:40:16 +0100

    might be buggy

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -15,21 +15,27 @@
       </div>
       <div class="anime-overview">
         <h3>Synopsis:</h3>
-          <p>{{ anime.attributes.synopsis | truncate(200) }}
-             <button @click="animeRoute(anime.id, anime.attributes.slug)">
-              read more
-            </button>
-          </p>   
+        <p>
+          {{ anime.attributes.synopsis | truncate(200) }}
+          <button @click="animeRoute(anime.id, anime.attributes.slug)">
+            read more
+          </button>
+        </p>
       </div>
     </div>
+    <back-to-top text="Back to top"></back-to-top>
   </div>
 </template>
 
 <script>
-import api from '../api'
+import api from "../api";
+import BackToTop from 'vue-backtotop'
 export default {
   name: "container",
   props: ["animes"],
+  components: {
+     BackToTop
+  },
   methods: {
     setRating(rating) {
       if (rating < 70) {
@@ -40,20 +46,22 @@ export default {
         return "green";
       }
     },
-    async animeRoute(id, animeName){
-        try {
-            this.$store.commit('animes/loading')
-            const res = await api.get(`anime/${id}`);
-            this.$store.commit('animes/animePage', res.data.data.attributes)
-            this.$router.push({name: 'anime', params: {
-            id:animeName 
-            }});
-            this.$store.commit('animes/loading')
-            this.$store.commit('animes/resetAnimes')
-        } catch (error) {
-            console.log(error)
-        }
-        
+    async animeRoute(id, animeName) {
+      try {
+        this.$store.commit("animes/loading");
+        const res = await api.get(`anime/${id}`);
+        this.$store.commit("animes/animePage", res.data.data.attributes);
+        this.$router.push({
+          name: "anime",
+          params: {
+            id: animeName,
+          },
+        });
+        this.$store.commit("animes/loading");
+        this.$store.commit("animes/resetAnimes");
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
